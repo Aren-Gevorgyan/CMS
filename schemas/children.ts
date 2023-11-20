78492b51efd898fe3c520fde15a89a970c71b6cc
Aren-Gevorgyan <areng4518@gmail.com>
2023-11-20 16:24:40 +0400

    Wrote all kind of fields

--- schemas/children.ts
+++ schemas/children.ts
@@ -1,46 +1,135 @@
+import { colorValidation, validation, validationDescription, validationImage, validationStrings } from "../common/validations";
+
 export default {
     name: 'children',
     type: 'document',
     title: 'Children',
     fields: [
       {
-        name: 'name',
+        name: 'kind',
         type: 'string',
-        title: 'Name',
+        title: 'Kind',
+        description: 'Towels, Robes ...',
+        validation: validation('Kind'),
       },
       {
-        name: 'nickName',
+        name: 'brand',
         type: 'string',
-        title: 'NickName',
+        title: 'Brand',
+        description: "HAMAM, L'APPARTEMENT ...",
+        validation: validation('Brand'),
       },
       {
-        name: 'slug',
-        type: 'slug',
-        title: 'Slug',
-        options: {
-          source: 'name',
-        },
+        name: 'title',
+        type: 'string',
+        title: 'Title',
+      },
+      {
+        name: 'description',
+        type: 'array',
+        title: 'Description',
+        of: [
+          {
+            title: 'Blog',
+            type: 'block',
+          },
+        ],
+        validation: validationDescription('Description'),
       },
       {
         name: 'image',
         type: 'image',
         title: 'Image',
-        options: {
-          //   hotspot: true,
-        },
+        validation: validationImage('Image'),
       },
-  
       {
-        name: 'bio',
+        name: 'availability',
+        type: 'number',
+        title: 'Availability',
+        description: 'Goods count 7,  2 ...',
+        validation: validation('Availability'),
+      },
+      {
+        name: 'price',
+        type: 'number',
+        title: 'Price',
+        validation: validation('Price'),
+      },
+      {
+        name: 'info',
+        title: 'Info',
         type: 'array',
-        title: 'Bio',
-        of: [
-          {
-            title: 'Blog',
-            type: 'block'
-          },
-          //   hotspot: true,
-        ],
+        description: 'Material: Satin, Страна: Россия ...',
+        of: [{type: 'string'}],
+      },
+      {
+        name: 'size',
+        title: 'Sizes',
+        type: 'array',
+        of: [{type: 'string'}],
+        validation: validationStrings('size'),
+      },
+      {
+        name: 'unavailableSizes',
+        title: 'Unavailable Sizes',
+        type: 'array',
+        of: [{type: 'string'}],
+      },
+      {
+        name: 'color',
+        title: 'Colors',
+        type: 'array',
+        description: 'black, #FFCC554 ...',
+        of: [{type: 'string'}],
+        validation: colorValidation,
+      },
+      {
+        name: 'unavailableColors',
+        title: 'Unavailable Colors',
+        type: 'array',
+        description: 'black, #FFCC554 ...',
+        of: [{type: 'string'}],
+      },
+      {
+        name: 'view',
+        title: 'Views',
+        type: 'array',
+        description: 'With fringe, Classical ...',
+        of: [{type: 'string'}],
+      },
+      {
+        name: 'unavailableView',
+        title: 'Unavailable Views',
+        type: 'array',
+        description: 'With fringe, Classical ...',
+        of: [{type: 'string'}],
+      },
+      {
+        name: 'type',
+        title: 'Type',
+        type: 'array',
+        description: 'Closed, Open ...',
+        of: [{type: 'string'}],
+      },
+      {
+        name: 'Type',
+        title: 'Unavailable Type',
+        type: 'array',
+        description: 'Closed, Open ...',
+        of: [{type: 'string'}],
+      },
+      {
+        name: 'gender',
+        title: 'Gender',
+        type: 'array',
+        of: [{type: 'string'}],
+        options: {
+          list: [
+            {title: 'Unisex', value: 'unisex'},
+            {title: "Women's", value: 'womens'},
+            {title: "Men's", value: 'mens'},
+          ],
+        },
       },
     ],
   }
